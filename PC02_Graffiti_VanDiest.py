f93e3ae97c552428a21c3624fce270ea1071b74b
benvandiest19 <70238759+benvandiest19@users.noreply.github.com>
2020-09-08 14:57:41 -0600

    Add files via upload

--- PC02_Graffiti_VanDiest.py
+++ PC02_Graffiti_VanDiest.py
@@ -9,43 +9,37 @@ Author: Ben Van Diest
 September 4, 2020
 PC02 Bezos Terminator
 '''
-from turtle import * #import the library of commands that you'd like to use
+from turtle import * 
 
 colormode(255)
 
-# Create a panel to draw on. 
-panel = Screen()
-w = 750 # width of panel
-h = 750 # height of panel
-panel.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
-#You can experiment by making it the size of your screen or super tiny!
 
-# Create a colorful background and add Bezos image to it
+panel = Screen()
+w = 750 
+h = 750 
+panel.setup(width=w, height=h) 
+
 image = "Bezos.gif"
 panel.bgcolor("black")
 panel.bgpic(image)
 
 up()
 
-#left shade
 goto(-22,112)
 shape("circle")
 shapesize(2,2.6,1)
 left(12)
 stamp()
 
-#right shade
 goto(47,120)
 stamp()
 
-#middle bar
 goto(11,125)
 shape("square")
 shapesize(.8,5.7,1)
 right(4)
 stamp()
 
-#side piece
 goto(-45,122)
 shape("circle")
 shapesize(.5,.5,1)
@@ -56,13 +50,11 @@ backward(45)
 right(164)
 forward(10)
 
-#red eye
 up()
 color(175,0,0)
 goto(47,120)
 stamp()
 
-#red eye streaks
 pensize(1.8)
 color("red4")
 down()
@@ -91,7 +83,6 @@ forward(20)
 forward(-20)
 up()
 
-#thought bubbles
 goto(85,70)
 color("WhiteSmoke")
 down()
@@ -108,7 +99,6 @@ down()
 circle(110)
 up()
 
-#money symbol
 goto(290,300)
 color("green4")
 down()
